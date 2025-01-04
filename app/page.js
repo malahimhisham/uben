bbb884b62575b39bf95d34fb5c7db1f4ea9a38b3
malahimhisham <malahimaxtra@gmail.com>
2025-01-04 10:53:04 +0500

    hello

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import LandingPage from "./Components/Home/LandingPage";
 export default function Home() {
   return (
     <div className="mt-24">
+      <h1>hello world</h1>
      <LandingPage/>
      <Cards/>
     </div>
